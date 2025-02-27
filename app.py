609064a6ecaeb0456eadae23508f663b082f4051
abelm19 <abelmetek19@gmail.com>
2025-02-27 18:16:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def main():
         return base64.b64encode(img_file.read()).decode("utf-8")
 
     # Local image path
-    image_path = '/Users/abelmetek/Desktop/Customer Churn Project/background1.jpg'  # Replace with your image path
+    image_path = 'background1.jpg'  # Replace with your image path
     img_base64 = img_to_base64(image_path)
 
     # Add CSS to set the background image
